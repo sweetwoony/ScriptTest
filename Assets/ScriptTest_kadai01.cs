ce2fd28fe18c7c7772e6ec6d3d79a4f32debf0c2
yuta.iseno <sweet_woony@hotmail.co.jp>
2018-03-26 17:32:48 +0900

    Lesson4 C#スクリプトの基礎　課題提出
    課題として出されたスクリプトを二つに分けてあります。
    「配列を駆使する課題」　を　「ScriptTest_kadai01」、　「BossクラスにMagic関数を加える」　を　「ScriptTest_kadai02」としました。
    「ScriptTest_kadai02」は非表示にしてあります。

---
+++ Assets/ScriptTest_kadai01.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptTest_kadai01 : MonoBehaviour {
+
+	void Start () {
+		// 配列arrayを宣言して好きな値で初期化
+		int[] array = { 51, 32, 43, 14, 75};
+
+
+		// 配列の各要素の値を順番に表示
+		for (int i = 0; i < array.Length; i++) {
+			Debug.Log ("順　" + (i+1) + "番目　＝　" + array [i]);
+		}
+
+		// for文を使い配列の各要素を逆順表示
+		for (int i = (array.Length-1); i >= 0; i--) {
+			Debug.Log ("逆　" + (i+1) + "番目　＝　" + array [i]);
+
+		}
+	}
+
+	void Update () {
+
+	}
+}
---
+++ Assets/ScriptTest_kadai02.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Boss{
+		private int hp = 100;
+		private int mp = 53;
+		private int power = 25;
+
+		public void Attack(){
+			Debug.Log(this.power + "のダメージを与えた！");
+		}
+
+		public void Defence(int damage){
+			Debug.Log(damage + "のダメージを受けた！");
+			this.hp -= damage;
+		}
+
+	//　BossクラスにMagic関数（魔法）を追加
+		public void Magic(int UseMagic){
+			if (this.mp >= UseMagic) {
+			this.mp -= UseMagic;
+			Debug.Log ("魔法を回使用した。残りＭＰは" + mp);	
+			}else{
+				Debug.Log ("ＭＰが足りないため魔法が使えない");
+		}
+	}
+}
+
+
+
+public class ScriptTest_kadai02 : MonoBehaviour {
+	
+
+		void Start () {
+			Boss lastboss = new Boss ();
+			lastboss.Attack ();
+			lastboss.Defence (3);
+
+		// 魔法複数使用のfor文を追加
+		for(int i = 0; i <= 10; i++){
+			lastboss.Magic (5);
+		}
+	}
+
+
+		void Update () {
+
+		}
+}
